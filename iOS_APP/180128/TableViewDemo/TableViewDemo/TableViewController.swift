b99edd4369ac5e3527339a2ac2d1ab7f6c8c3922
KamilHung <3619466@yahoo.com.tw>
2018-01-28 16:11:43 +0800

    Multi Cell
    Use 2 Array section and cell,
    and delete function

--- iOS_APP/180128/TableViewDemo/TableViewDemo/TableViewController.swift
+++ iOS_APP/180128/TableViewDemo/TableViewDemo/TableViewController.swift
@@ -10,21 +10,37 @@ import UIKit
 
 class TableViewController: UITableViewController {
     
-    var cityList = ["Taipei", "Zhongli", "Taizhong", "Tainan", "Khaosiung"]
+    var cityList = [
+        ["Taipei", "Zhongli", "Taizhong", "Tainan", "Khaosiung"],
+        ["New York", "Los Angeles", "Chicago", "Seattle", "Houston"],
+        ["London", "Munich", "Budapest", "Rome", "Moscow"]  ]
+    
+    var sectionNames = ["Taiwan", "US", "Europe"]
     
     override func numberOfSections(in tableView: UITableView) -> Int {
-        return 1 //Section number
+        return cityList.count //Section number: 3 Array
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return cityList.count //Array size
+        return cityList[section].count //One section size
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "My Cell", for: indexPath)
         
-        cell.textLabel?.text = cityList[indexPath.row]
+        cell.textLabel?.text = cityList[indexPath.section][indexPath.row]
         return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? { //Show to Header
+        return sectionNames[section]
+    }
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) { //Edit Cell
+        if editingStyle == .delete { //Delete Cell
+            cityList[indexPath.section].remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        }
     }
 }
 
